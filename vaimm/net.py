6dba43f45096b253003108e3d30af607a1f24816
Jonas Tingeborn <tinjon@gmail.com>
2023-08-04 23:15:38 +0200

    Fix HTTP error handling
    * Proper rate-limiting response if retry-after is provided for 429
    * More aggressive default backoff in case retry-after isn't provided
    * Capture number of failed downloads statistics
    * Report reason for failed file downloads

--- vaimm/net.py
+++ vaimm/net.py
@@ -13,15 +13,6 @@ from .version import __version__
 DOWNLOAD_URL_PREFIX = 'https://veai-models.topazlabs.com/'
 
 def download_model_files(missing_files:Iterable[str], threads:int, cookie:str):
-    def assert_download_success(future) -> int:  # bytes written
-        try:
-            return future.result()  # Just check that no error was generated during the download
-        except BaseException as e:
-            if isinstance(e, KeyboardInterrupt):
-                raise
-            print(f'Download failed: {e}', file=sys.stderr)
-            sys.exit(1)
-
     def scale_unit(nbytes:int) -> str:
         if nbytes > 1<<30:
             return f'{nbytes/(1<<30):.2f} GiB'
@@ -42,16 +33,24 @@ def download_model_files(missing_files:Iterable[str], threads:int, cookie:str):
         started_at = time.time() - 0.00001
         total_bytes_downloaded = 0
         elapsed = 0
+        failed = []
 
         try:
             with tqdm(total=len(futures), unit=' files ', desc='Downloading', leave=True) as pbar:
                 for future in as_completed(futures):
-                    total_bytes_downloaded += assert_download_success(future)
+                    file = futures[future]
+                    exc = future.exception()
+
+                    if exc:
+                        failed.append((file, exc))
+                    else:
+                        total_bytes_downloaded += future.result()
+
                     elapsed = time.time() - started_at
                     bps = total_bytes_downloaded / elapsed
                     speed = f'{scale_unit(bps)}/s'
                     pbar.update(1)
-                    pbar.set_postfix({'speed': speed})
+                    pbar.set_postfix({'speed': speed, 'failed': len(failed)})
 
         except KeyboardInterrupt:
             for future in futures:
@@ -59,10 +58,42 @@ def download_model_files(missing_files:Iterable[str], threads:int, cookie:str):
             executor.shutdown(wait=False)
             raise
 
-    print(f'Download completed successfully with {scale_unit(total_bytes_downloaded)} of data fetched.')
+    if failed and len(failed) <= len(futures):
+        print(f'Download completed, {len(failed)} out of {len(futures)} files failed to download.')
+        print('\nThe failed downloads:')
+        for file, error in failed:
+            print(f'  {os.path.basename(file)}  (failure reason: {error})')
+    else:
+        print(f'Download completed successfully with {scale_unit(total_bytes_downloaded)} of data fetched.')
 
 
-def retry(max_tries=5, initial_wait_secs=1, backoff=3):
+def retry(max_tries=4, initial_wait_secs=4, backoff=4):
+    retriable_codes = {408, 423, 429, 500, 502, 503, 504, 507, 599}
+
+    def find_retry_after_seconds(headers:dict):
+        s = headers.get('Retry-After')
+        if not s:
+            return None
+        if s.isdigit():
+            return int(s)
+
+        # in date format: <day-name>, <day> <month> <year> <hour>:<minute>:<second> GMT
+        t = time.strptime(s, '%a, %d %b %Y %H:%M:%S %Z')
+        delta_secs = time.mktime(t) - time.mktime(time.gmtime())
+        return delta_secs
+
+    def handle_http_error(exc:HTTPError) -> bool:
+        if exc.code in retriable_codes:
+            delay_secs = find_retry_after_seconds(dict(exc.headers))
+            if delay_secs:
+                print(f'Intermittent error: {exc}, Retrying in {delay_secs} seconds '
+                      'as per server ask...')
+                time.sleep(delay_secs)
+                return True  # already delayed, so let caller know not to delay further
+            return False     # no specific delay asked from server; apply standard retry logic
+        else:
+            raise exc  # terminal fault, not a retriable http error
+
     def decorator(func):
         @wraps(func)
         def inner(*args, **kw):
@@ -70,11 +101,16 @@ def retry(max_tries=5, initial_wait_secs=1, backoff=3):
             for attempt in range(max_tries):
                 try:
                     return func(*args, **kw)
+
                 except (HTTPError, ContentTooShortError) as exc:
-                    if isinstance(exc, HTTPError) and 400 <= exc.code < 500 and exc.code not in (408,429):
-                        raise  # don't retry on file not found, or invalid cf-cookie
                     if attempt == max_tries - 1:
-                        raise
+                        raise exc  # too many retries
+
+                    if isinstance(exc, HTTPError):
+                        delayed = handle_http_error(exc)
+                        if delayed:
+                            continue
+
                     print(f'Error encountered during download: {exc}, Retrying in {wait_duration} seconds...',
                           file=sys.stderr)
                     time.sleep(wait_duration)
@@ -83,7 +119,7 @@ def retry(max_tries=5, initial_wait_secs=1, backoff=3):
     return decorator
 
 
-@retry(max_tries=5)
+@retry()
 def download_file(url:str, cookie:str, filename:str, chunk_size:int=8<<10) -> int:
     request = Request(url, headers={
         'cookie': f'cf_clearance={cookie}',
